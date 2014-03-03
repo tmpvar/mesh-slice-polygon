1b4abdf17c1bf2d3c41d9fad5c7b646a67a8c015
Elijah Insua <tmpvar@gmail.com>
2014-03-03 15:49:37 -0800

    functional

--- mesh-slice-polygon.js
+++ mesh-slice-polygon.js
@@ -1,12 +1,291 @@
-var mesh-slice-polygon = function() {
-  
-}
+(function() {
+  function Vertex(x, y, z) {
+    if (x instanceof Float32Array) {
+      this.position = x;
+    } else if (Array.isArray(x)) {
+      this.position = vec3.fromValues(x[0], x[1], x[2]);
+    } else {
+      this.position = vec3.fromValues(x, y, z);
+    }
 
+    this.id = Vertex.id++;
+  }
 
-if (typeof module !== "undefined" && typeof module.exports == "object") {
-  module.exports = mesh-slice-polygon;
-}
+  Vertex.id = 0;
 
-if (typeof window !== "undefined") {
-  window.mesh-slice-polygon = window.mesh-slice-polygon || mesh-slice-polygon;
-}
+  Vertex.toString = function(x, y, z) {
+    if (x instanceof Float32Array) {
+      return '(' + [x[0], x[1], x[2]].join(',') + ')'
+    } else {
+      if (!Array.isArray(x)) {
+        x = [x, y, z];
+      }
+      return '(' + x.join(',') + ')'
+    }
+  };
+
+  Vertex.prototype = {
+    near : function(vert, threshold) {
+      return vec3.dist(this.position, vert.position) < (threshold || 0.1)
+    },
+
+    toString : function() {
+      return Vertex.toString(this.position);
+    }
+  };
+
+  function Triangle(a, b, c, normal) {
+    this.verts = [a, b, c];
+    this.normal = normal;
+
+    this.sortByZ();
+    this.id = Triangle.id++;
+  }
+
+  Triangle.id = 0;
+
+  Triangle.prototype.sortByZAscending = function(a, b) {
+    return a.position[2] > b.position[2] ? -1 : 1;
+  };
+
+  Triangle.prototype.sortByZ = function() {
+    // TODO SORT: reverse sort if milling/printing
+    this.verts.sort(this.sortByZAscending);
+  };
+
+  Triangle.prototype.toString = function() {
+    return '[' + this.id + ':' + [this.verts[0].id, this.verts[1].id, this.verts[2].id].join(',') + ']';
+  };
+
+  function ZPlane(sliceZ) {
+    this.position = vec3.fromValues(0,0,sliceZ);
+    this.normal = vec3.fromValues(0, 0, 1);
+  }
+
+  ZPlane.prototype.intersect = function(start, end) {
+    var tmp = vec3.fromValues(0, 0, 0)
+    var num = vec3.dot(
+      this.normal,
+      vec3.subtract(
+        vec3.fromValues(0, 0, 0),
+        this.position,
+        start.position
+      )
+    );
+
+    var line = vec3.subtract(
+      vec3.fromValues(0, 0, 0),
+      end.position,
+      start.position
+    );
+
+    var den = vec3.dot(this.normal,line);
+
+    var res = num/den;
+    if (!isNaN(res) && 0 <= res && res <= 1.0) {
+      var isect = vec3.add(
+        vec3.fromValues(0, 0, 0),
+        start.position,
+        vec3.multiply(
+          vec3.fromValues(0, 0, 0),
+          line,
+          vec3.fromValues(res, res, res)
+        )
+      );
+
+      return isect;
+    }
+  };
+
+  function MeshSlicePolygon() {
+    this.plane = new ZPlane(sliceZ)
+    this.seenVerts = {};
+    this.sharedTriangles = {};
+    this.triangles = [];
+    this.bounds = {
+      max : [-Infinity, -Infinity, -Infinity],
+      min : [Infinity, Infinity, Infinity]
+    };
+
+    this.groups = [];
+    this.group = [];
+  }
+
+  MeshSlicePolygon.prototype.trackBounds = function(coords) {
+    var bounds = this.bounds;
+
+    for (var i = 0; i<3; i++) {
+      if (coords[i] > bounds.max[i]) {
+        bounds.max[i] = coords[i];
+      }
+
+      if (coords[i] < bounds.min[i]) {
+        bounds.min[i] = coords[i];
+      }
+    }
+  };
+
+  MeshSlicePolygon.prototype.upsertVert = function(x, y, z) {
+    var key = Vertex.toString(x, y, z);
+
+    if (!this.seenVerts[key]) {
+      this.seenVerts[key] = new Vertex(x, y, z);
+      this.trackBounds(this.seenVerts[key].position);
+    }
+
+    return this.seenVerts[key];
+  }
+
+  MeshSlicePolygon.prototype.sharedTriangle = function(a, b, ignore) {
+    var aa = this.sharedTriangles[a.id];
+    var ab = this.sharedTriangles[b.id];
+
+    for (var i = 0; i<aa.length; i++) {
+      var ai = aa[i];
+      for (var j = 0; j<ab.length; j++) {
+        if (ai.id === ab[j].id && ignore.indexOf(ai.id) === -1) {
+          return ai;
+        }
+      }
+    }
+
+    return false;
+  };
+
+  MeshSlicePolygon.prototype.addTriangle = function(a, b, c) {
+    this.dirty = true;
+    var a = this.upsertVert(a[0], a[1], a[2]);
+    var b = this.upsertVert(b[0], b[1], b[2]);
+    var c = this.upsertVert(c[0], c[1], c[2]);
+
+    var triangle = new Triangle(a, b, c);
+
+    // TODO: we don't really need a loop here...
+
+    [a, b, c].forEach(function(vertex) {
+      if (!this.sharedTriangles[vertex.id]) {
+        this.sharedTriangles[vertex.id] = [];
+      }
+
+      this.sharedTriangles[vertex.id].push(triangle);
+    }.bind(this));
+
+    this.triangles.push(triangle);
+  };
+
+  var sortShared = function(a, b) {
+    return (a.id > b.id) ? -1 : 1;
+  };
+
+  MeshSlicePolygon.prototype.sort = function() {
+    Object.keys(this.sharedTriangles).forEach(function(key) {
+      this.sharedTriangles[key].sort(sortShared);
+    }.bind(this));
+
+    this.triangles.sort(function(a, b) {
+      return (a.verts[0].position[2] < b.verts[0].position[2]) ? -1 : 1;
+    });
+  };
+
+  var isectTests = [[0,1], [0, 2], [1, 2]];
+
+  MeshSlicePolygon.prototype.followIntersectionChain = function(tri, startTri) {
+    this.group = [];
+    var last = startTri;
+    while (tri) {
+
+      if (this.seenTriangles[tri.id]) {
+        break;
+      }
+
+      this.seenTriangles[tri.id] = true;
+
+      var isects = [];
+      for (var i=0; i<isectTests.length; i++) {
+        var test = isectTests[i];
+        var isect = this.plane.intersect(tri.verts[test[0]], tri.verts[test[1]])
+        if (isect) {
+          var vert = this.upsertVert(isect);
+          vert.shared = test;
+          isects.push(vert);
+        }
+      }
+
+      if (isects.length === 3) {
+        console.log('PARALLEL',
+          this.sharedTriangles[tri.verts[0].id].length,
+          this.sharedTriangles[tri.verts[1].id].length,
+          this.sharedTriangles[tri.verts[2].id].length
+        );
+        break;
+      } else if (isects.length === 2) {
+        this.group.push(isects[0]);
+
+        var shared = this.sharedTriangle(
+          tri.verts[isects[0].shared[0]],
+          tri.verts[isects[0].shared[1]],
+          [tri.id, last, startTri]
+        );
+
+        if (!shared) {
+          shared = this.sharedTriangle(
+            tri.verts[isects[1].shared[0]],
+            tri.verts[isects[1].shared[1]],
+            [tri.id, last, startTri]
+          );
+        }
+
+        if (shared && shared.id !== tri.id && shared.id !== startTri) {
+          last = tri.id;
+          tri = shared;
+        } else {
+          if (this.group.length > 0) {
+            this.groups.push(this.group);
+            this.group = [];
+          }
+          break;
+        }
+      } else {
+        break;
+      }
+    }
+  };
+
+  MeshSlicePolygon.prototype.slice = function(z) {
+    this.plane.position[2] = z;
+
+    if (this.dirty) {
+      this.dirty = false;
+      this.sort();
+    }
+
+    var l = this.triangles.length;
+    this.groups = [];
+    this.group = [];
+
+    this.seenTriangles = {};
+    while (l--) {
+      var startTri = this.triangles[l].id;
+
+      if (!this.seenTriangles[startTri]) {
+        var triVerts = this.triangles[l].verts;
+
+        if (triVerts[0].position[2] >= z) {
+          this.followIntersectionChain(this.triangles[l], startTri);
+        } else {
+          break;
+        }
+      }
+    }
+
+    return this.groups;
+  };
+
+  if (typeof module !== "undefined" && typeof module.exports == "object") {
+    module.exports = MeshSlicePolygon;
+  }
+
+  if (typeof window !== "undefined") {
+    window.MeshSlicePolygon = window.MeshSlicePolygon || MeshSlicePolygon;
+  }
+})()
